656092273c97d9e6bdfe27ee4c104d67335f3a47
Vinay-2014 <Kalakotadinesh2014@gmail.com>
2026-05-11 12:52:20 +0530

    Update server to listen on all interfaces

--- server.js
+++ server.js
@@ -40,4 +40,6 @@ app.post('/download', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`);
+});
